efaaa21c154c564e50bcbefc069dadef8195b7cc
Juan José Contardo <juan.contardo@gmail.com>
2024-04-03 22:10:53 -0300

    Update Library.hs

--- src/Library.hs
+++ src/Library.hs
@@ -5,4 +5,3 @@ interes:: Number -> String -> Number
 interes montoDepositado nombre 
   | montoDepositado > 1000 = min (montoDepositado * 0.1) 500
   | montoDepositado > 500 = 10 * length nombre
-  | otherwise = montoDepositado / 7 
